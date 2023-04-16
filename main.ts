c9b2a0b6f75ccbab4b2229fdabb9a5849f14ec25
wzhixin321 <125794407+wzhixin321@users.noreply.github.com>
2023-04-16 16:05:24 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,21 @@
 let myImage = 0
-input.onButtonPressed(Button.A, function () {
-    myImage = (31 - 24) % 7
-    if (myImage == 1) {
-        basic.showString("Monday")
-    } else if (myImage == 2) {
-        basic.showString("Tuesday")
-    } else if (myImage == 3) {
-        basic.showString("Wednesday ")
-    } else if (myImage == 4) {
-        basic.showString("Thursday ")
-    } else if (myImage == 5) {
-        basic.showString("Friday")
-    } else if (myImage == 6) {
-        basic.showString("Saturday")
-    } else {
-        basic.showString("Sunday")
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        myImage = 155 % 7
+        if (myImage == 1) {
+            basic.showString("Saturday")
+        } else if (myImage == 2) {
+            basic.showString("Sunday")
+        } else if (myImage == 3) {
+            basic.showString("Monday")
+        } else if (myImage == 4) {
+            basic.showString("Tuesday")
+        } else if (myImage == 5) {
+            basic.showString("Wednesday ")
+        } else if (myImage == 6) {
+            basic.showString("Thursday ")
+        } else {
+            basic.showString("Friday")
+        }
     }
 })
